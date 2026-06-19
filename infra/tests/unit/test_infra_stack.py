88cdd9faa660992859fb003d6bb7783365145010
unknown <hpablobenja@gmail.com>
2026-06-19 11:15:19 -0400

    Producto Final

--- infra/tests/unit/test_infra_stack.py
+++ infra/tests/unit/test_infra_stack.py
@@ -2,7 +2,8 @@ import aws_cdk as cdk
 from aws_cdk import assertions
 from infra.infra_stack import TicketFlowInfraStack
 
-#test
+
+# test
 def test_sqs_queue_created():
     app = cdk.App()
     stack = TicketFlowInfraStack(app, "infra")
